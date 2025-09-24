8856bcd3fcbe8396d0703665b0a76caf9e507d80
daniels <danielsv2008@gmail.com>
2025-09-24 12:10:16 +0300

    Es padodos nekas neiet

--- submit.php
+++ submit.php
@@ -2,13 +2,21 @@
 $servername = "localhost";
 $username = "book_review_user_24092025";
 $password = "password";
-
-$conn = new mysqli($servername, $username, $password);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+$dbname = "book_review_24092025";
+    $full_name = "full_name";
+    $book_title = "book_title";
+    $review = "review_text";
+    $rating = "rating";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO book_review (full_name, book_title,review_text, rating)
+  VALUES ('$full_name', '$book_title', '$review', $rating)";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
 }
-?> 
+$conn = null;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
